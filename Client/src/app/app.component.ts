13ea6eff305071369727b3461771c48c44ca4d62
RasifKhan <rasifkhanrasu@gmail.com>
2025-04-05 15:40:01 +0600

    End of section 9

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -4,29 +4,40 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  baseUrl= 'https://localhost:5001/api/'
-  private http=inject(HttpClient)
-  title = 'Skinet';
-  //products: any[] = [];
-  products: Product[] = [];
+export class AppComponent {
+//   //baseUrl= 'https://localhost:5001/api/'     //removesd after configuraing shop.service.ts
+//   //private http=inject(HttpClient)            ////removesd after configuraing shop.service.ts
 
-  ngOnInit(): void {
-   // this.http.get<any>(this.baseUrl + 'products').subscribe
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response =>this.products = response.data,
-      error: error =>console.log(error),
-      complete: () => console.log('complete')
-    })
+//  private shopService=inject (ShopService);
+
+//   title = 'Skinet';
+//   //products: any[] = [];
+//   products: Product[] = [];
+
+//   ngOnInit(): void {
+//    // this.http.get<any>(this.baseUrl + 'products').subscribe
+//    // this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({   //////removesd after configuraing shop.service.ts
+//    this.shopService.getProducts().subscribe({ 
+//       next: response =>this.products = response.data,
+//       error: error =>console.log(error),
+      
+//       //complete: () => console.log('complete')
+//     })
     
-    
-  }
+  
+//   }
+
+title = 'Skinet';
+
+
 }
